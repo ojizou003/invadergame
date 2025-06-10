99d94a575b2360f8bf623159f1cdbf433d5cc5e1
ojizou003 <sinissi@gmail.com>
2025-06-10 22:38:34 +0900

    ひとまず完成

--- src/graphics/Sprite.ts
+++ src/graphics/Sprite.ts
@@ -1,2 +1,28 @@
-import { Renderer } from \"./Renderer\";\n\nexport class Sprite {\n    // Placeholder class for sprite management\n    // Will be used later for drawing actual invader/player images\n    private image: HTMLImageElement | null = null;\n    public width: number = 0;\n    public height: number = 0;\n\n    constructor(src: string) {\n        this.image = new Image();\n        this.image.onload = () => {\n            this.width = this.image!.width;\n            this.height = this.image!.height;\
-        };\n        this.image.src = src;\n    }\n\n    // Basic draw method - will be more sophisticated later\n    draw(renderer: Renderer, x: number, y: number): void {\n        if (this.image && this.image.complete) {\n            // Ensure drawing at integer coordinates for pixel-perfect rendering\n            const pixelX = Math.floor(x);\n            const pixelY = Math.floor(y);\n            renderer.getContext().drawImage(this.image, pixelX, pixelY, this.width, this.height);\n        }\n    }\n}
+import { Renderer } from "./Renderer";
+
+export class Sprite {
+    // Placeholder class for sprite management
+    // Will be used later for drawing actual invader/player images
+    private image: HTMLImageElement | null = null;
+    public width: number = 0;
+    public height: number = 0;
+
+    constructor(src: string) {
+        this.image = new Image();
+        this.image.onload = () => {
+            this.width = this.image!.width;
+            this.height = this.image!.height;
+        };
+        this.image.src = src;
+    }
+
+    // Basic draw method - will be more sophisticated later
+    draw(renderer: Renderer, x: number, y: number): void {
+        if (this.image && this.image.complete) {
+            // Ensure drawing at integer coordinates for pixel-perfect rendering
+            const pixelX = Math.floor(x);
+            const pixelY = Math.floor(y);
+            renderer.getContext().drawImage(this.image, pixelX, pixelY, this.width, this.height);
+        }
+    }
+}
